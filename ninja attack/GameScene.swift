f35379f43bb163925bd82b9e5c5b747736f55048
eduardo <rl238513@stu.rlas-116.org>
2019-05-29 11:18:07 -0500

    kk

--- ninja attack/GameScene.swift
+++ ninja attack/GameScene.swift
@@ -33,7 +33,6 @@ class GameScene: SKScene {
         return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
     }
 
-    
     func random(min: CGFloat, max: CGFloat) -> CGFloat {
         return random() * (max-min) + min
     }
